8adc1033e6a13c85f0abd8aaaad3921601629521
13R1ch13 <richaa0aard@gmail.com>
2025-08-14 14:28:37 +0300

    Handle invalid POLL_INTERVAL

--- config.py
+++ config.py
@@ -1,9 +1,21 @@
 import os
+import logging
 from dotenv import load_dotenv
 
 load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "")
-POLL_INTERVAL = int(os.getenv("POLL_INTERVAL", "15"))
+
+# POLL_INTERVAL should be an integer number of seconds
+_poll_interval = os.getenv("POLL_INTERVAL", "15")
+try:
+    POLL_INTERVAL = int(_poll_interval)
+except ValueError:
+    logging.error(
+        "Invalid POLL_INTERVAL '%s'. Expected an integer. Using default of 15 seconds.",
+        _poll_interval,
+    )
+    POLL_INTERVAL = 15
+
 DB_PATH = os.getenv("DB_PATH", ".data/bot.db")
 BINANCE_API = os.getenv("BINANCE_API", "https://api.binance.com")
